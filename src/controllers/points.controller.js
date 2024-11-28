f1196fe21d78f82a46ef0d44edad92498d4f1a6c
Bilal <bilalrahim012@gmail.com>
2024-11-27 22:29:20 -0600

    final

--- src/controllers/points.controller.js
+++ src/controllers/points.controller.js
@@ -1,22 +1,34 @@
-
 const { addPoints: addPointsService, spendPoints: spendPointsService, getPointsBalance: getPointsBalanceService} = require('../services/points.service');
 
+/**
+ * Handles the request to add points for a given payer at a specified timestamp.
+ * 
+ * @param {Object} req - The request object containing the body with payer, points, and timestamp.
+ * @param {Object} res - The response object used to send the response back to the client.
+ * @returns {void} - Sends a status response, no return value.
+ */
 const addPoints = (req, res) => {
   const { payer, points, timestamp } = req.body;
-
   if (!payer || !points || !timestamp) {
-    return res.status(400).json({ success: false, message: '[request-body-incomplete]'})
+    return res.status(400).json({ success: false, message: '[request-body-incomplete]'});
   }
 
-  addPointsService(payer, points, timestamp);  
-
+  addPointsService(payer, points, timestamp);
+  
   return res.status(200).send();
 };
 
+/**
+ * Handles the request to spend points. If the user has enough points, it deducts them from the oldest transactions.
+ * 
+ * @param {Object} req - The request object containing the body with the points to be spent.
+ * @param {Object} res - The response object used to send the response back to the client.
+ * @returns {void} - Sends a response with the result of the spend operation.
+ */
 const spendPoints = (req, res) => {
   const { points } = req.body;
   if (!points) {
-    return res.status(400).json({ success: false, message: '[request-body-incomplete]'})
+    return res.status(400).json({ success: false, message: '[request-body-incomplete]'});
   }
 
   const deductions = spendPointsService(points);
@@ -24,13 +36,19 @@ const spendPoints = (req, res) => {
     return res.status(200).json([deductions?.data]);
   }
 
-  res.status(400).send(deductions?.message)
-  
+  res.status(400).send(deductions?.message);
 };
 
+/**
+ * Retrieves the current balance of points for each payer.
+ * 
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object used to send the response back to the client.
+ * @returns {void} - Sends the current points balance for each payer.
+ */
 const getPointsBalance = (req, res) => {
-
   const pointsBalance = getPointsBalanceService();
+  
   res.status(200).json(pointsBalance);
 };
 
--- src/services/points.service.js
+++ src/services/points.service.js
@@ -1,70 +1,86 @@
 const { writeDataToFile, readAllFiles, readDataFromFile } = require('../utils/file.utils');
 const path = require('path');
-
+const FILEPATH = path.resolve(__dirname, '../../datastore')
+/**
+ * Adds points to a specific payer at a given timestamp.
+ * 
+ * @param {string} payer - The name of the payer.
+ * @param {number} points - The number of points to add.
+ * @param {string} timestamp - The timestamp of when the transaction takes place.
+ * @returns {void} - No return value.
+ */
 const addPoints = (payer, points, timestamp) => {
-  const filePath = path.resolve(__dirname, `../../datastore/${timestamp}.json`);  
-  writeDataToFile(filePath, { payer, points, timestamp })
-
+  const filePath = `${FILEPATH}/${timestamp}.json`;  
+  writeDataToFile(filePath, { payer, points, timestamp });
   return;
 }
 
+/**
+ * Deducts points from the oldest transactions until the specified spend amount is reached.
+ * 
+ * @param {number} spendAmount - The total amount of points to spend.
+ * @returns {Object} - The result of the spend operation, including success and deducted points by payer.
+ * @returns {boolean} result.success - Whether the operation was successful.
+ * @returns {string} [result.message] - A message indicating the failure reason (if any).
+ * @returns {Object} [result.data] - An object showing the deductions per payer, with the payer as the key and deducted points as the value.
+ */
 const spendPoints = (spendAmount) => {
-  const datastorePath = path.resolve(__dirname, "../../datastore");
+  const transactionFiles = readAllFiles(FILEPATH);
 
-    const transactionFiles = readAllFiles(datastorePath);
-    const transactions = transactionFiles.map((file) => {
-      const filePath = path.join(datastorePath, file);
-      return readDataFromFile(filePath);
-    });
-    
-    const totalPoints = transactions.reduce((acc, { points }) => acc + points, 0);
-    if (spendAmount > totalPoints) {
-      return { success: false, message: "[Not-Enough-Points]" };
-    }
+  const transactions = transactionFiles.map((file) => {
+    const filePath = path.join(FILEPATH, file);
+    return readDataFromFile(filePath);
+  });
 
-    transactions.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
-    
-    let pointsToSpend = spendAmount;
-    const deductions = {};
+  const totalPoints = transactions.reduce((acc, { points }) => acc + points, 0);
 
-    transactions.forEach((transaction) => {
-      if (pointsToSpend <= 0) return;
-    
-      const { payer, points, timestamp } = transaction;
-    
-      if (points <= 0) return;
-    
-      const pointsToDeduct = Math.min(points, pointsToSpend);
-      pointsToSpend -= pointsToDeduct;
-      transaction.points -= pointsToDeduct;
-    
-      deductions[payer] = (deductions[payer] || 0) - pointsToDeduct;
-    
-      const filePath = path.join(datastorePath, `${timestamp}.json`);
-      writeDataToFile(filePath, transaction);
-    });
-    
+  if (spendAmount > totalPoints) {
+    return { success: false, message: "[Not-Enough-Points]" };
+  }
 
-    return { success: true, data: deductions };
+  transactions.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
+  
+  let pointsToSpend = spendAmount;
+  const deductions = {};
+
+  transactions.forEach((transaction) => {
+    if (pointsToSpend <= 0) return;
+
+    const { payer, points, timestamp } = transaction;
+
+    const pointsToDeduct = Math.min(points, pointsToSpend);
+    pointsToSpend -= pointsToDeduct;
+    transaction.points -= pointsToDeduct;
+
+    deductions[payer] = (deductions[payer] || 0) - pointsToDeduct;
+
+    const filePath = path.join(FILEPATH, `${timestamp}.json`);
+    writeDataToFile(filePath, transaction);
+  });
+
+  return { success: true, data: deductions };
 }
 
+/**
+ * Retrieves the current points balance for each payer.
+ * 
+ * @returns {Object} - A mapping of each payer to their current points balance.
+ * @returns {string} payer - The name of the payer.
+ * @returns {number} payerBalance - The total points balance for that payer.
+ */
 const getPointsBalance = () => {
-  const datastorePath = path.resolve(__dirname, "../../datastore");
-
   const payerBalances = {};
 
-  const files = readAllFiles(datastorePath);
+  const files = readAllFiles(FILEPATH);
 
-    files.forEach((file) => {
-      const filePath = path.join(datastorePath, file);
-      const transaction = readDataFromFile(filePath)
+  files.forEach((file) => {
+    const filePath = path.join(FILEPATH, file);
+    const transaction = readDataFromFile(filePath);
+    const { payer, points } = transaction;
+    payerBalances[payer] = (payerBalances[payer] || 0) + points;
+  });
 
-      const { payer, points } = transaction;
-
-      payerBalances[payer] = (payerBalances[payer] || 0) + points;
-    });
-
-    return payerBalances;
+  return payerBalances;
 }
 
 module.exports = {
